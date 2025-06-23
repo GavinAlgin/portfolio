b9e53c12122f8d978c8244b7f89dab3124d4041d
Gavin Algin <gavinkidart65@gmail.com>
2025-06-23 21:44:25 +0200

    firebase config

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'portfolioga-83b3e',
   storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_MSG_ID',
   appId: 'YOUR_APP_ID',
